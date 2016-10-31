52a5befcc5e8086b691b37b688c142665fd4e155
glynester <glynester@hotmail.com>
2016-10-31 16:24:45 +0000

    Added ||= to allow rspec to use test db. Set up sessions routes.

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,7 @@
-ENV['RACK_ENV'] = 'development'
+ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
+require 'sinatra/flash'
 require './app/models/user'
 require './app/data_mapper_setup'
 
@@ -9,7 +10,7 @@ class MakersBnB < Sinatra::Base
 
   enable :sessions
   set :session_secret, 'super secret'
-  # register Sinatra::Flash
+  register Sinatra::Flash
   helpers do
     def current_user
       @current_user ||= User.get(session[:user_id])
@@ -44,6 +45,32 @@ class MakersBnB < Sinatra::Base
       erb :'/users/new'
     end
   end
+
+
+  get '/sessions/sign_in' do
+      erb :'sessions/sign_in'
+    end
+
+    post '/sessions' do
+      user = User.authenticate(params[:email], params[:password])
+      if user
+        session[:user_id] = user.id
+        redirect '/home'
+      else
+        flash.now[:errors] = ['The email or password is incorrect']
+        erb :'/home'
+      end
+    end
+
+    delete '/sessions' do
+      session[:user_id] = nil
+      session[:nickname] = nil
+      flash.keep[:notice] = "Goodbye!"
+      redirect '/home'
+    end
+
+
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
