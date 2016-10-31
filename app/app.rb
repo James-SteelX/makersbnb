04d255cfbb1d010cb3ad7e84432a022058e96906
James <James.Steel@me.com>
2016-10-31 15:16:02 +0000

    clean up

--- app/app.rb
+++ app/app.rb
@@ -1,19 +1,10 @@
-ENV['RACK_ENV'] ||= 'development'
+ENV['RACK_ENV'] = 'development'
 
 require 'sinatra/base'
-require_relative 'models/user'
 require_relative 'data_mapper_setup'
 
+
 class MakersBnB < Sinatra::Base
-
-  enable :sessions
-  set :session_secret, 'super secret'
-
-  helpers do
-    def current_user
-      @current_user ||= User.get(session[:user_id])
-    end
-  end
 
   get '/' do
     'Hello World'
@@ -24,17 +15,12 @@ class MakersBnB < Sinatra::Base
   end
 
   post '/sign_up' do
-    user = User.create(first_name: params[:first_name],
+    User.create(first_name: params[:first_name],
                 last_name: params[:last_name],
                 email: params[:email],
                 password: params[:password],
                 password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
     redirect '/home'
-  end
-
-  get '/home' do
-    erb :home
   end
 
   # start the server if ruby file executed directly
