e54e3c68f4c5e0e15871d29853a41398d747a5e1
Liam Taylor <liam.taylor888@googlemail.com>
2016-10-31 14:06:51 +0000

    1) Created user form (/view/users/new.erb) 2) Created failing feature test for sign up 3) Created web_helpers.rb and helpr for sign_up 4) Created Rakefile and add auto_migrate and auto_update tasks 5) Hashed out database cleaner areas in spec_helper to try and trouble shoot issue Liam has  when running Rspec 6) Moved ENV[RACK_ENV] line ip app.rb folder to top of script 7) Created routes in app.rb for '/users/new' and 'post/sign_up'

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,8 @@
-require 'sinatra/base'
-require_relative 'data_mapper_setup.rb'
-
 ENV['RACK_ENV'] = 'development'
+
+require 'sinatra/base'
+require_relative 'data_mapper_setup'
+
 
 class MakersBnB < Sinatra::Base
 
@@ -9,7 +10,19 @@ class MakersBnB < Sinatra::Base
     'Hello World'
   end
 
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/sign_up' do
+    User.create(first_name: params[:first_name],
+                last_name: params[:last_name],
+                email: params[:email],
+                password: params[:password],
+                password_confirmation: params[:password_confirmation])
+    redirect '/home'
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
-
 end
