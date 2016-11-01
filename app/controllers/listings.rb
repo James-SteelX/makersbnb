2138a50b07a0c6b33dd79555e6749170f04de568
James <James.Steel@me.com>
2016-11-01 13:43:30 +0000

    users prveted from posting without beign signed in, tested and working

--- app/controllers/listings.rb
+++ app/controllers/listings.rb
@@ -1,15 +1,23 @@
 class MakersBnB < Sinatra::Base
 
   get '/listing/new' do
-    erb :'listing/new'
+    if current_user != nil
+     erb :'listing/new'
+   else
+     flash.keep[:errors] = ['Please sign in to post a listing']
+     redirect('/sessions/sign_in')
+   end
   end
 
   post '/listing' do
     listing = Listing.new(street_address: params[:street_address], city: params[:city], description: params[:description], price: params[:price])
     user = current_user
     listing.user = user
-    listing.save
-    redirect to('/listing/confirmation')
+    if listing.save
+       redirect to('/listing/confirmation')
+    else
+      'sorry no good'
+    end
   end
 
   get '/listing/confirmation' do
--- spec/features/create_listing_spec.rb
+++ spec/features/create_listing_spec.rb
@@ -5,7 +5,6 @@ require_relative 'listing_web_helpers'
 feature 'Listing a property on makersbnb' do
   scenario 'a signed in user can visit listing page' do
     sign_up
-    # sign_in
     click_link('List a property')
     expect(current_path).to eq('/listing/new')
     expect(page).to have_content('Create a property listing')
@@ -13,10 +12,22 @@ feature 'Listing a property on makersbnb' do
 
   scenario 'user can create a listing' do
     sign_up
-    create_listing
+    expect{create_listing}.to change(Listing, :count).by(1)
     expect(current_path).to eq('/listing/confirmation')
     expect(page).to have_content('Your home has been successfully listed')
   end
+
+  scenario 'you cant see the link if not logged in' do
+    visit('/')
+    expect(page).not_to have_content('List a property')
+  end
+
+  scenario 'you cant create a listing if not logged in' do
+    visit('/listing/new')
+    expect(current_path).to eq('/sessions/sign_in')
+    expect(page).to have_content('Please sign in to post a listing')
+  end
+
 
 
 
