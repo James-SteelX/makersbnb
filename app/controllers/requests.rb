d4c3e3de3e82f04704a80ed2f15653a11e25b25d
glynester <glynester@hotmail.com>
2016-11-01 21:47:43 +0000

    Interim solution new requests. Can get value into DB but listing ID not coming through.

--- app/controllers/requests.rb
+++ app/controllers/requests.rb
@@ -1,10 +1,26 @@
 class MakersBnB < Sinatra::Base
 
-  get '/requests/new' do
+  post '/requests/new' do
+    p @listing_id = params[:listing_id]
     erb :'requests/new'
   end
 
   post '/requests/confirmation' do
+    request = Request.new(start_date: params[:start_date],
+    end_date: params[:end_date])
+
+    #  listing = Listing.first(id: params[:listing_id])
+    listing = Listing.first(id: 3)
+    request.listing = listing
+    if request.save
+      redirect to('/requests/confirmation')
+    else
+      'Sorry your message was not sent'
+    end
+  end
+
+  get '/requests/confirmation' do
     erb :'requests/confirmation'
   end
+
 end
---
+++ app/models/request.rb
@@ -0,0 +1,14 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+require 'dm-migrations'
+
+class Request
+  include DataMapper::Resource
+
+  property :id,          Serial
+  property :start_date,  Date, required: true
+  property :end_date,    Date, required: true
+  property :status,      String, default: :"for_review"
+
+  belongs_to :listing
+end
