4cca280220e813f797cf71d8d83899e59814ac90
James <James.Steel@me.com>
2016-11-04 18:27:55 +0000

    fixed sign up email

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -14,9 +14,9 @@ class MakersBnB < Sinatra::Base
                 password_confirmation: params[:password_confirmation])
     if @user.save
       session[:user_id] = @user.id
+      Email.sign_up(@user.email)
       flash.keep[:notice] = "Welcome, #{@user.first_name}"
       redirect('/home')
-      Email.sign_up(@user.email)
     else
       flash.now[:errors] = @user.errors.full_messages
       erb :'/users/new'
